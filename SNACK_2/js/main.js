9ef2a94a171e5b52aecaa29994efe5bcccc9d727
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-08 16:30:24 +0100

    complete snack-2

--- SNACK_2/js/main.js
+++ SNACK_2/js/main.js
@@ -2,3 +2,8 @@
     A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
     Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
 */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const squaredNumbers = numbers.map ( number => number * number);
+console.log(numbers);
+console.log(squaredNumbers);
